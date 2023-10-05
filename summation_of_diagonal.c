e41d6421c576c3c0c1b787ef0a2295d48b7bd366
adityatripathi676 <77868902+adityatripathi676@users.noreply.github.com>
2023-10-05 06:22:34 +0000

    created summation.c

---
+++ summation_of_diagonal.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main() {
+    int a[50][50],row,col,i,j,sum=0;
+    printf("Enter the Number of rows :");
+    scanf("%d",&row);
+    printf("Enter the number of Columns: ");
+    scanf("%d",&col);
+    for ( i = 0; i < row; i++)
+    {
+        for ( j = 0; j < col; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+    printf("The matrix entered is: \n");
+    for ( i = 0; i < row; i++)
+    {
+        for ( j = 0; j < col; j++)
+        {
+            printf("%d \t",a[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0; i<row; i++) {
+        for ( j = 0; j < col; j++)
+        {
+            if (i==j) {
+                sum = sum + a[i][j];
+            }
+        }
+        
+    }
+    printf("Sum is: %d",sum);
+    return 0;
+}
